777df7f759a2fc6d90c7a39c55b5f168f3696c9f
Mantik495 <ricardo.visini@students.unibe.ch>
2014-11-02 21:05:25 +0100

    Pure Databank classes like in trello wanted, small changes possibly needed when implemented...

---
+++ Skeleton/src/main/java/org/sample/controller/pojos/EnquiryForm.java
@@ -0,0 +1,50 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package org.sample.controller.pojos;
+
+
+public class EnquiryForm {
+    
+    
+private String enquiryTitle;
+private String enquiryText;
+
+private String username;
+private String adNumber;
+
+
+public String getUsername() {
+return username;
+}
+public void setUsername(String username) {
+this.username = username;
+}
+
+public String getAdNumber() {
+return adNumber;
+}
+
+public void setAdNumber(String adNumber) {
+this.adNumber = adNumber;
+}
+
+public String getEnquiryTitle() {
+return enquiryTitle;
+}
+
+public void setEnquiryTitle(String enquiryTitle) {
+this.enquiryTitle = enquiryTitle;
+}
+
+public String getEnquiryText() {
+return enquiryText;
+}
+
+public void setEnquiryText(String enquiryText) {
+this.enquiryText = enquiryText;
+}
+
+}
