e4b7c41f1824f04935b41e978a6a4e0014df0ff2
Mantik495 <ricardo.visini@students.unibe.ch>
2014-11-02 19:31:02 +0100

    No two equivalent Emails in our database bug fixed

--- Skeleton/src/main/java/org/sample/model/dao/UserDao.java
+++ Skeleton/src/main/java/org/sample/model/dao/UserDao.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserDao extends JpaRepository<User, Long> {
 
 	User findByUsername(String username);
+        
+        User findByEmail(String email);
 
 }
+
