9ae2e690c8ec3c7c9becb9574e14a358c95290b7
Severin Zumbrunn <sz@tune-x.ch>
2014-11-03 15:22:54 +0100

    changed Enquirydao and Enquiry.java

--- Skeleton/src/main/java/org/sample/model/Enquiry.java
+++ Skeleton/src/main/java/org/sample/model/Enquiry.java
@@ -17,60 +17,69 @@ import javax.persistence.OneToOne;
 */
 @Entity
 public class Enquiry {
-    
-@Id
-@GeneratedValue
-private Long id;
-/*Title is for inserting name of logged in person who writes*/
+	    
+	@Id
+	@GeneratedValue
+	private Long id;
+	/*Title is for inserting name of logged in person who writes*/
+	
+	private String enquiryTitle;
 
-private String enquiryTitle;
-private String enquiryText;
+	private String enquiryText;
+	
+	/*At least to receiver and person who wrote it...*/
+	@OneToOne
+	private User userFrom;
+	@OneToOne
+	private User userTo;
+	
+	/*It is fused with one advert*/
+	@OneToOne
+	private Advert advert;
+	
+	
+	public Long getId() {
+	return id;
+	}
+	public void setId(Long id) {
+	this.id = id;
+	}
+	public String getEnquiryTitle() {
+	return enquiryTitle;
+	}
+	
+	public void setEnquiryTitle(String enquiryTitle) {
+	this.enquiryTitle = enquiryTitle;
+	}
+	
+	public String getEnquiryText() {
+	return enquiryText;
+	}
+	
+	public void setEnquiryText(String enquiryText) {
+	this.enquiryText = enquiryText;
+	}
+	
+	public Advert getAdvert() {
+	return advert;
+	}
+	
+	public void setAdvert(Advert advert) {
+	this.advert = advert;
+	}
+	
+	public User getUserFrom() {
+		return userFrom;
+	}
+	public void setUserFrom(User userFrom) {
+		this.userFrom = userFrom;
+	}
+	public User getUserTo() {
+		return userTo;
+	}
+	public void setUserTo(User userTo) {
+		this.userTo = userTo;
+	}
 
-/*At least to receiver and person who wrote it...*/
-@OneToOne
-private User user;
-
-/*It is fused with one advert*/
-@OneToOne
-private Advert advert;
-
-
-public Long getId() {
-return id;
-}
-public void setId(Long id) {
-this.id = id;
-}
-public String getEnquiryTitle() {
-return enquiryTitle;
-}
-
-public void setEnquiryTitle(String enquiryTitle) {
-this.enquiryTitle = enquiryTitle;
-}
-
-public String getEnquiryText() {
-return enquiryText;
-}
-
-public void setEnquiryText(String enquiryText) {
-this.enquiryText = enquiryText;
-}
-
-public Advert getAdvert() {
-return advert;
-}
-
-public void setAdvert(Advert advert) {
-this.advert = advert;
-}
-
-public User getUser() {
-return user;
-}
-
-public void setUser(User user) {
-this.user = user;
-}
 
 }
--- Skeleton/src/main/java/org/sample/model/dao/EnquiryDao.java
+++ Skeleton/src/main/java/org/sample/model/dao/EnquiryDao.java
@@ -4,7 +4,7 @@ package org.sample.model.dao;
 import org.sample.model.Enquiry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EnquiryDAO extends JpaRepository<Enquiry, Long> {
+public interface EnquiryDao extends JpaRepository<Enquiry, Long> {
     
 Enquiry findbyId(long id);
 
