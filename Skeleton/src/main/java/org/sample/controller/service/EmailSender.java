108ac15eb6f56fc9383b04980a4a0857dc6e01da
Mantik495 <ricardo.visini@students.unibe.ch>
2014-11-26 23:36:48 +0100

    In between stage

---
+++ Skeleton/src/main/java/org/sample/controller/service/EmailSender.java
@@ -0,0 +1,38 @@
+package org.sample.controller.service;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import javax.mail.internet.MimeMessage;
+import org.sample.model.Notifies;
+import org.sample.model.User;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+/**
+ *This class sends the E-mails to the accounts to announce an notification
+ * 
+ */
+public class EmailSender {
+    
+    
+public void GenerateAnEmail(Notifies notifies) throws MessagingException {
+
+JavaMailSenderImpl sender = new JavaMailSenderImpl(); 
+sender.setHost("mail.host.com");
+
+MimeMessage message = sender.createMimeMessage();
+MimeMessageHelper helper = new MimeMessageHelper(message);
+String username = notifies.getToUser().getUsername();
+helper.setTo(username);
+helper.setText(notifies.getText());
+
+sender.send(message);
+}    
+    
+}
